3817bd4982a0258b3493c269fc80eaa784636781
Anish S <38997845+git-anish@users.noreply.github.com>
2023-07-03 23:51:27 -0700

    Update openai.ts removing duplicates
    Update openai.ts removing duplicates and updating model names

--- types/openai.ts
+++ types/openai.ts
@@ -10,9 +10,7 @@ export interface OpenAIModel {
 export enum OpenAIModelID {
   GPT_3_5 = 'gpt-3.5-turbo',
   GPT_3_5_16K = 'gpt-3.5-turbo-16k',
-  GPT_3_5_AZ = 'gpt-35-turbo',
   GPT_4 = 'gpt-4',
-  GPT_4_0314 = 'gpt-4-0314',
   GPT_4_32K = 'gpt-4-32k',
 }
 
@@ -26,11 +24,11 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
     maxLength: 12000,
     tokenLimit: 4000,
   },
-  [OpenAIModelID.GPT_3_5_AZ]: {
-    id: OpenAIModelID.GPT_3_5_AZ,
-    name: 'GPT-3.5',
-    maxLength: 12000,
-    tokenLimit: 4000,
+  [OpenAIModelID.GPT_3_5_16K]: {
+    id: OpenAIModelID.GPT_3_5_16K,
+    name: 'GPT-3.5-16K',
+    maxLength: 96000,
+    tokenLimit: 16000,
   },
   [OpenAIModelID.GPT_4]: {
     id: OpenAIModelID.GPT_4,
@@ -38,21 +36,9 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
     maxLength: 24000,
     tokenLimit: 8000,
   },
-  [OpenAIModelID.GPT_4_0314]: {
-    id: OpenAIModelID.GPT_4_0314,
-    name: 'GPT-4-0314',
-    maxLength: 24000,
-    tokenLimit: 8000,
-  },
   [OpenAIModelID.GPT_4_32K]: {
     id: OpenAIModelID.GPT_4_32K,
     name: 'GPT-4-32K',
-    maxLength: 96000,
-    tokenLimit: 32000,
-  },
-  [OpenAIModelID.GPT_3_5_16K]: {
-    id: OpenAIModelID.GPT_3_5_16K,
-    name: 'GPT-3.5-16K',
     maxLength: 96000,
     tokenLimit: 32000,
   }
